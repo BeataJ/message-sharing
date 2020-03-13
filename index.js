b01279ef8dc7130a639e8ea906e6a0dd02955710
beata <beata@emc2wave.com>
2020-03-13 16:02:45 -0700

    add message

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { hash } = window.location;
 
-atob(hash.replace('#', ''));
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = message;
+}
 
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
